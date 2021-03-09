fc64cc1567c7dc6807c8bd9c9067b2f5620435e9
JoaquinCerdan <jjcl4@alu.ua.es>
2021-03-09 20:58:08 +0100

    Model Client realizado

---
+++ database/migrations/2021_03_09_195338_create_clients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->bigIncrements('numCliente');
+            $table->timestamps();
+            $table->String('nombreCompleto');
+            $table->integer('tiempoEmpleado');
+            $table->String('taquillaActual');
+            $table->String('direccion');
+            $table->String('numCuenta');
+            $table->integer('numTelefono');
+            $table->Date('fechaAlta');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
