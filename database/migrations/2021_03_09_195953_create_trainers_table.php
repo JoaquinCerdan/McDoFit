1acbb3cbf0edcea8742339df38b3e34d4393a5b9
JoaquinCerdan <jjcl4@alu.ua.es>
2021-03-09 21:03:52 +0100

    Model Trainer realizado

---
+++ database/migrations/2021_03_09_195953_create_trainers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainers', function (Blueprint $table) {
+            $table->bigIncrements('numEntrenador');
+            $table->timestamps();
+            $table->String('nombreCompleto');
+            $table->String('direccion');
+            $table->String('numCuenta');
+            $table->integer('numTelefono');
+            $table->String('especialidad');
+            $table->String('turno');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainers');
+    }
+}
