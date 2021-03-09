79cfd127887d46d0f579822a942f154173cfb582
JoaquinCerdan <jjcl4@alu.ua.es>
2021-03-09 21:09:00 +0100

    Model Machine realizado

---
+++ database/migrations/2021_03_09_200459_create_machines_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMachinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('machines', function (Blueprint $table) {
+            $table->bigIncrements('identificador');
+            $table->timestamps();
+            $table->String('nombre');
+            $table->String('sala');
+            $table->integer('vecesUtilizada');
+            $table->boolean('disponible');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('machines');
+    }
+}
